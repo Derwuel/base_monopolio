fc6d7f847540ec439c5e54fd37fe2a92fd4d15dc
marcos <ma.hernandezo@duocuc.cl>
2024-10-26 19:51:37 -0300

    segundo modelo listo

--- mono-01/src/mono_01/pipelines/calculo_mono/pipeline.py
+++ mono-01/src/mono_01/pipelines/calculo_mono/pipeline.py
@@ -5,11 +5,11 @@ generated using Kedro 0.19.9
 
 from kedro.pipeline import Pipeline, pipeline, node
 
-from .nodes import split_data2, mod_regresion_logistica,evaluacion_regresion_logistica, mod_arbol_decision, evaluacion_arbol_decision, mod_svc, evaluacion_svc
+from .nodes import  split_data2, mod_SGDClassifier, evaluacion_SGDClassifier, mod_tree_model_clasicicacion, evaluacion_tree_model_clasicicacion
 
 
-def create_pipeline(**kwargs):
-    return Pipeline([
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([
         node(
             func=split_data2,
             inputs=["pos_proceso", "params:model_options2"],
@@ -17,33 +17,24 @@ def create_pipeline(**kwargs):
             name="split_data_node_02"
         ),
         node(
-            func=mod_regresion_logistica,
+            func=mod_SGDClassifier,
             inputs=["X_train_clasi", "y_train_clasi"],
-            outputs="logistic_model",
+            outputs="model_SGDClassifier",
         ),
         node(
-            func=evaluacion_regresion_logistica,
-            inputs=["logistic_model", "X_test_clasi", "y_test_clasi"],
+            func=evaluacion_SGDClassifier,
+            inputs=["model_SGDClassifier", "X_test_clasi", "y_test_clasi"],
             outputs=None,
         ),
-        node(
-            func=mod_arbol_decision,
+                node(
+            func=mod_tree_model_clasicicacion,
             inputs=["X_train_clasi", "y_train_clasi"],
-            outputs="decision_tree_model",
+            outputs="model_tree_clasicicacion",
         ),
         node(
-            func=evaluacion_arbol_decision,
-            inputs=["decision_tree_model", "X_test_clasi", "y_test_clasi"],
+            func=evaluacion_tree_model_clasicicacion,
+            inputs=["model_tree_clasicicacion", "X_test_clasi", "y_test_clasi"],
             outputs=None,
         ),
-        node(
-            func=mod_svc,
-            inputs=["X_train_clasi", "y_train_clasi"],
-            outputs="svc_model",
-        ),
-        node(
-            func=evaluacion_svc,
-            inputs=["svc_model", "X_test_clasi", "y_test_clasi"],
-            outputs=None,
-        ),
+
     ])
--- mono-01/src/mono_01/settings.py
+++ mono-01/src/mono_01/settings.py
@@ -2,6 +2,7 @@
 from the Kedro defaults. For further information, including these default values, see
 https://docs.kedro.org/en/stable/kedro_project_setup/settings.html."""
 
+
 # Instantiated project hooks.
 # For example, after creating a hooks.py and defining a ProjectHooks class there, do
 # from mono_01.hooks import ProjectHooks
